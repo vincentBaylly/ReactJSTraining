2353072afd66a16a80ac187c5c966e3d1d428a0b
vincentBaylly <vincent.baylly@gmail.com>
2021-09-18 14:12:53 -0400

    Event propagation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,31 @@
+function click() {
+  var div1 = document.getElementById("div1");
+  //ajouter un troisieme parametre a addEventListener pour controler l'ordre d'execution de vos evenements
+  div1.addEventListener(
+    "click",
+    function () {
+      alert("div clique");
+    }
+    //,false
+  );
+  var p2 = document.getElementById("p2");
+  //ajouter un troisieme parametre a addEventListener pour controler l'ordre d'execution de vos evenements
+  p2.addEventListener(
+    "click",
+    function () {
+      alert("paragraphe clique");
+    }
+    //,true
+  );
+}
+
 function Home() {
-  return <div>Bienvenue dans la boutique</div>;
+  return (
+    <div id="div1">
+      <p id="p1">paragraphe 1</p>
+      <p id="p2">paragraphe 2</p>
+    </div>
+  );
 }
 
 export default Home;
