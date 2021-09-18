c44753539e74f34948111c5147334c31445d53e3
vincentBaylly <vincent.baylly@gmail.com>
2021-09-18 14:48:10 -0400

    add a productList to our Shop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Title from "./components/Title";
 import Description from "./components/Description";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Title name="John Doe" />
       <Description />
-      <Description />
-      <Description />
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,7 @@
-function click() {
-  var div1 = document.getElementById("div1");
-  //ajouter un troisieme parametre a addEventListener pour controler l'ordre d'execution de vos evenements
-  div1.addEventListener(
-    "click",
-    function () {
-      alert("div clique");
-    }
-    //,false
-  );
-  var p2 = document.getElementById("p2");
-  //ajouter un troisieme parametre a addEventListener pour controler l'ordre d'execution de vos evenements
-  p2.addEventListener(
-    "click",
-    function () {
-      alert("paragraphe clique");
-    }
-    //,true
-  );
-}
+import ProductList from "./ProductList";
 
 function Home() {
-  return (
-    <div id="div1">
-      <p id="p1">paragraphe 1</p>
-      <p id="p2">paragraphe 2</p>
-    </div>
-  );
+  return <ProductList />;
 }
 
 export default Home;
