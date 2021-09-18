9000f3b7d4c5efd68e65887bc5bad5385201737d
vincentBaylly <vincent.baylly@gmail.com>
2021-09-18 12:28:17 -0400

    migrate your component from function to classes

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,9 @@
-function Title() {
-  return <h1>Titre du site</h1>;
+import React from "react";
+
+class Title extends React.Component {
+  render() {
+    return <h1>Titre du site</h1>;
+  }
 }
 
 export default Title;
